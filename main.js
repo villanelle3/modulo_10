de299eb1bc62f5d071729e3eca6c3d309108f7aa
Laura Augusta <laurah.algusta@gmail.com>
2023-02-01 13:16:45 -0300

    updating my branch

--- main.js
+++ main.js
@@ -7,4 +7,53 @@ $(document).ready(function(){
         $("nav").slideToggle()
     })
 
+    // let mascara if (celular) ? (00) 00000-0000 : xxxxxx
+
+    $("#tel").mask("(00) 00000-0000", {
+        placeholder: "(00) 00000-0000",
+    })
+
+    $("form").validate({
+        rules:{
+            name:{
+                required: true
+            },
+            email:{
+                required: true, 
+                email: true
+            },
+            tel:{
+                required: true
+            },
+            intere:{
+                required: true
+            },
+        },
+        messages:{
+            name: "Este campo é obrigatório."
+        },
+        submitHandler: function(form){
+            console.log(form)
+        },
+        inalidHandler: function(event, validad){
+            let nincorrect = validad.numberOfInvalids();
+            if (nincorrect){
+                console.log(`${event} is incorrect due ${nincorrect}`)
+            }         
+
+        }
+    })
+
+    $(".items-list button").click(function(){
+        const name =  $(this).parent().find("h3").text()
+        const destino = $("#contact")
+        $("#intere").val(name)
+        $("html").animate({
+            scrollTop: destino.offset().top,
+        }, 1000)
+    })
+
 });
+
+// mask https://igorescobar.github.io/jQuery-Mask-Plugin/
+// validation https://jqueryvalidation.org/
